72de6d58ebe359ac43629c602fc877d07656b263
koerner-axs <54717715+koerner-axs@users.noreply.github.com>
2019-08-30 18:07:15 +0200

    Indentation problems resolved

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ let net;
 async function app() {
 	// Init
 	$('.image-section').hide();
-    $('.loader').hide();
-    $('#result').hide();
+	$('.loader').hide();
+	$('#result').hide();
 
-    // Image Selection Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                document.getElementById('imagePreview').src = e.target.result;
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#imageSelection").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
-        readURL(this);
-    });
+	// Image Selection Preview
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				document.getElementById('imagePreview').src = e.target.result;
+				$('#imagePreview').hide();
+				$('#imagePreview').fadeIn(650);
+			}
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+	$("#imageSelection").change(function () {
+		$('.image-section').show();
+		$('#btn-predict').show();
+		$('#result').text('');
+		$('#result').hide();
+		readURL(this);
+	});
 
 	// Load the model
 	console.log('Loading mobilenet..');
-    $('.loader').show();
+	$('.loader').show();
 	net = await mobilenet.load();
 	console.log('Successfully loaded model');
-    $('.loader').hide();
+	$('.loader').hide();
 
-    // Prediction
+	// Prediction
 	$('#btn-predict').click(async function () {
 		$(this).hide();
 		$('.loader').show();
@@ -47,7 +47,7 @@ async function app() {
 		console.log(prediction);
 		$('#result').text(" Predicted class: " + prediction[0]['className'] + "\nProbability: " + prediction[0]['probability']);
 		$('#result').show();
-    });
+	});
 }
 
 app();
